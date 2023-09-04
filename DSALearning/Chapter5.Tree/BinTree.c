9d666d282171dd557e6794713ac2a6e7bd664b0a
sharkycake <3084213727@qq.com>
2023-09-04 21:59:32 +0800

    Order in BinTree

--- DSALearning/Chapter5.Tree/BinTree.c
+++ DSALearning/Chapter5.Tree/BinTree.c
@@ -47,3 +47,41 @@ int IndexSameAncestor(int i ,int j){
     }
     return i;
 }
+
+//---------遍历二叉树--------
+void visit(BinTree B);
+//前序遍历
+void PreOrder(BinTree B){
+    if(B !=NULL){
+        visit(B);
+        PreOrder(B->lchild);
+        PreOrder(B->rchild);
+    }
+}
+//中序和后序遍历都是改变Visit的位置
+
+//利用辅助队列按层次遍历二叉树
+//定义辅助队列
+typedef struct
+{
+    BiTNode* data;   //数据域
+    struct LinkNode * next;    //后驱元素指针
+}LinkNode;
+
+//真正的链式队列只包含一个头指针与一个尾指针
+typedef struct
+{
+    LinkNode * front , *rear;   //定义的时候不能忘记rear前面的*
+}*LinkQueue;
+
+void FloorOrder(BinTree B){
+    LinkQueue Q;
+    Q->front->data = B;
+    while(Q!=NULL){
+        //1.将队列头部R元素出队
+        //2.并对头部元素进行访问
+        if(Q->front->data->rchild!=NULL){
+            //将左右孩子入队
+        }
+    }
+}
