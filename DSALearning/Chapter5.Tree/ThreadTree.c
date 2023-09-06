33625b4bf425f012a5f712009e68d10deadd47d9
sharkycake <3084213727@qq.com>
2023-09-06 19:26:01 +0800

    Throughout the ThreadTree in medium sequence

--- DSALearning/Chapter5.Tree/ThreadTree.c
+++ DSALearning/Chapter5.Tree/ThreadTree.c
@@ -14,6 +14,8 @@
 ThreadNode* pre ;
 ThreadNode* p;  //used to refer present node
 
+//线索化
+//对于先序遍历要注意避免出现循环问题
 //对中序遍历序列进行线索化
 //对结点进行线索化
 void visit(ThreadNode *A){
@@ -49,4 +51,29 @@ ThreadTree* CreateInThread(ThreadTree T){
         pre->rtag = 1;
     }
     return T;
+}
+
+//中序线索二叉树的遍历
+//寻找后继节点（分为两种情况）
+//p—>rtag == 1 时 p的后继结点就是rchild
+//p->rtag == 0 时 1.若右子树为0，则后继节点为NULL，2若右子树不为0，则右子树中最左边的元素即为结点的后继元素
+
+//求线索二叉树中中序序列下的第一个结点
+ThreadNode* FirstNode(ThreadTree T){
+    while(T->ltag == 0) T = T->lchild;
+    return T;
+}
+//求线索二叉树中中序序列的后继结点
+ThreadNode* NextNode(ThreadNode* p){
+    if(p->rtag == 0 ){
+        return FirstNode(p->rchild);
+    }else return p->rchild; //p->rtag == 1 的情况 ,即rtag指向的就是后继节点
+}
+
+//According to the two functions above , Throughout the BinTree
+void Inorder(ThreadTree T){
+    ThreadNode  * a = NULL;
+    for (a = T;a!=NULL;a = NextNode(a)) {
+        visit(a);
+    }
 }
