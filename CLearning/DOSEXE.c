a3ee4d90b6ce10737a3241125e5d300fb2bc870a
sharkycake <3084213727@qq.com>
2024-04-09 15:48:19 +0800

    Update DOSEXE.c

--- CLearning/DOSEXE.c
+++ CLearning/DOSEXE.c
@@ -2,8 +2,8 @@
 // Created by 86150 on 2024/3/11.
 //
 #include "stdio.h"
-
+#define  CR printf("\n")
 int main(){
-    double x = 1;
-    printf("\\TOP\t\65\"");
+    int a = 1,b = 4,c = 3;
+    printf("%d",-02|12);
 }
